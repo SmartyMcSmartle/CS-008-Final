9a2db60855ae11aa3983f37ca08c7f973b591bcb
Elizabeth Bambury <Elizabeth>
2017-11-15 12:21:51 -0500

    Need to re-commit files after accidentaly editing final doc, back to normal now

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <!-- Home Page --!>
-<?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- top.php
+++ top.php
@@ -1,9 +1,2 @@
 <!-- Top of every page on RR --!>
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
