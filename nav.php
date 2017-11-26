a0c71dc535cdb6bca9bb9926b5cdd1ba4324edb4
Julia Beatty <jybeatty@uvm.edu>
2017-11-26 16:07:38 -0500

    wanted to add a link for countries but realized it's not a file.

--- nav.php
+++ nav.php
@@ -2,51 +2,51 @@
 <!-- there needs to be a link to countries, china, france, india, japan, mexico, us, faq, contact us, and about us --!>
 <!-- Start of Nav -->
 <nav>
-        <?php
-        if ($path_parts['filename'] == 'china') {
-            print 'activePage';
-        }
-        print '<a href="countries/china/china.php">China</a>';
+    <?php
+    if ($path_parts['filename'] == 'china') {
+        print 'activePage';
+    }
+    print '<a href="countries/china/china.php">China</a>';
 
-        if ($path_parts['filename'] == 'france') {
-            print 'activePage';
-        }
-        print '<a href="countries/france/france.php">France</a>';
+    if ($path_parts['filename'] == 'france') {
+        print 'activePage';
+    }
+    print '<a href="countries/france/france.php">France</a>';
 
-        if ($path_parts['filename'] == 'india') {
-            print 'activePage';
-        }
-        print '<a href="countries/india/india.php">India</a>';
+    if ($path_parts['filename'] == 'india') {
+        print 'activePage';
+    }
+    print '<a href="countries/india/india.php">India</a>';
 
-        if ($path_parts['filename'] == 'japan') {
-            print 'activePage';
-        }
-        print '<a href="countries/japan/japan.php">Japan</a>';
+    if ($path_parts['filename'] == 'japan') {
+        print 'activePage';
+    }
+    print '<a href="countries/japan/japan.php">Japan</a>';
 
-        if ($path_parts['filename'] == 'mexico') {
-            print 'activePage';
-        }
-        print '<a href="countries/mexico/mexico.php">Mexico</a>';
+    if ($path_parts['filename'] == 'mexico') {
+        print 'activePage';
+    }
+    print '<a href="countries/mexico/mexico.php">Mexico</a>';
 
-        if ($path_parts['filename'] == 'us') {
-            print 'activePage';
-        }
-        print '<a href="countries/us/us.php">USA</a>';
+    if ($path_parts['filename'] == 'us') {
+        print 'activePage';
+    }
+    print '<a href="countries/us/us.php">USA</a>';
 
-        if ($path_parts['filename'] == 'faq') {
-            print 'activePage';
-        }
-        print '<a href="faq.php">FAQ</a>';
+    if ($path_parts['filename'] == 'faq') {
+        print 'activePage';
+    }
+    print '<a href="faq.php">FAQ</a>';
 
-        if ($path_parts['filename'] == 'contact') {
-            print 'activePage';
-        }
-        print '<a href="contact/contact.php">Contact Us</a>';
+    if ($path_parts['filename'] == 'contact') {
+        print 'activePage';
+    }
+    print '<a href="contact/contact.php">Contact Us</a>';
 
-        if ($path_parts['filename'] == 'about') {
-            print 'activePage';
-        }
-        print '<a href="about.php">About Us</a>';
-        ?>
+    if ($path_parts['filename'] == 'about') {
+        print 'activePage';
+    }
+    print '<a href="about.php">About Us</a>';
+    ?>
 </nav>
 <!-- End of Nav -->
