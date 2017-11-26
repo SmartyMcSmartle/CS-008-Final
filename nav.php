79026f59a696bc205fd63c253e51903ea00f49c0
Julia Beatty <jybeatty@uvm.edu>
2017-11-26 16:04:42 -0500

    Fixed nav.php and added css to nav so the links are vertical and is fixed position. Added a link for countries.

--- nav.php
+++ nav.php
@@ -2,53 +2,51 @@
 <!-- there needs to be a link to countries, china, france, india, japan, mexico, us, faq, contact us, and about us --!>
 <!-- Start of Nav -->
 <nav>
-    <ol>
         <?php
-        if ($path_parts['filename'] == 'China') {
+        if ($path_parts['filename'] == 'china') {
             print 'activePage';
         }
         print '<a href="countries/china/china.php">China</a>';
 
-        if ($path_parts['filename'] == 'France') {
+        if ($path_parts['filename'] == 'france') {
             print 'activePage';
         }
         print '<a href="countries/france/france.php">France</a>';
 
-        if ($path_parts['filename'] == 'India') {
+        if ($path_parts['filename'] == 'india') {
             print 'activePage';
         }
         print '<a href="countries/india/india.php">India</a>';
 
-        if ($path_parts['filename'] == 'Japan') {
+        if ($path_parts['filename'] == 'japan') {
             print 'activePage';
         }
         print '<a href="countries/japan/japan.php">Japan</a>';
 
-        if ($path_parts['filename'] == 'Mexico') {
+        if ($path_parts['filename'] == 'mexico') {
             print 'activePage';
         }
         print '<a href="countries/mexico/mexico.php">Mexico</a>';
 
-        if ($path_parts['filename'] == 'USA') {
+        if ($path_parts['filename'] == 'us') {
             print 'activePage';
         }
         print '<a href="countries/us/us.php">USA</a>';
 
-        if ($path_parts['filename'] == 'FAQ') {
+        if ($path_parts['filename'] == 'faq') {
             print 'activePage';
         }
         print '<a href="faq.php">FAQ</a>';
 
-        if ($path_parts['filename'] == 'Contact Us') {
+        if ($path_parts['filename'] == 'contact') {
             print 'activePage';
         }
         print '<a href="contact/contact.php">Contact Us</a>';
 
-        if ($path_parts['filename'] == 'About Us') {
+        if ($path_parts['filename'] == 'about') {
             print 'activePage';
         }
         print '<a href="about.php">About Us</a>';
         ?>
-    </ol>
 </nav>
 <!-- End of Nav -->
