85d31eada9a91d0a00f6d528f50075fff788576a
SmartyMcSmartle <smartymcsmartle@gmail.com>
2017-11-27 09:52:13 -0500

    all recipes will now link to one page, recipe-detail (which is recipeTemplate renamed). This means that all of the individual country and recipe pages are unneccesary and I deleted them. There needs to be a lot of css done in order to properly display the images, and we also need to figure out how we are going to format the csv so that we can read in the ingredients. We also need to figure out how to convert variables to strings and back without losing any data.

--- countries/recipe-detail.php
+++ countries/recipe-detail.php
@@ -1,7 +1,7 @@
 <?php
-$country = "";
-if (isset($_GET['country'])) {
-    $country = htmlentities($_GET['country'], ENT_QUOTES, "UTF-8");
+$recipe = "";
+if (isset($_GET['recipe'])) {
+    $recipe = htmlentities($_GET['recipe'], ENT_QUOTES, "UTF-8");
 }
 // *** Open data *** //
 $debug = false;
@@ -55,27 +55,33 @@ if ($file) {
         print '</pre></p>';
     }
 } // ends if file was opened 
-
 fclose($file);
+
+$image_src = "../images/" . $recipe . ".jpg";
 include('../top.php');
 ?>
-<article id="recipes">
-    <h2><?php print $country;?> Recipes</h2>
-    <?php
-        foreach ($recipeDetails as $recipeDetail) {
-            if ($country == str_replace(' ', '', $recipeDetail[1])) {
-                print '<a href="recipe-detail.php?recipe=';
-                print str_replace(' ', '', $recipeDetail[0]);
-                print '">';
-                print $recipeDetail[0];
-                print '</a>';
-            }
-        }   
-    ?>
-</article>
+        <article>
+            <h2><?php print $recipe ?></h2>
+            <figure class="img-right">
+                <img alt="Recipe" src=<?php print $image_src ?>>
+                <figcaption>Source</figcaption>
+            </figure>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+            </ul>
+            <h3>Instructions</h3>
+            <ol>
+                <li>First step</li>
+                <li>Second step</li>
+                <li>Third step</li>
+            </ol>
+        </article>
+    </body>
+</html>
 <?php
 include('../footer.php');
 ?>
-</body>
-</html>
-
