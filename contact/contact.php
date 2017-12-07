b4ed4fa6501b7a3ab2e259a27ba14d6c52d49b84
Elizabeth Bambury <Elizabeth>
2017-12-07 10:13:20 -0500

    Added content to the Contact Us page (contact.php), it now includes links to the three form pages

--- contact/contact.php
+++ contact/contact.php
@@ -1,9 +1,17 @@
-<!-- link to subscription form, recipe form, and comments form --!>
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include('../top.php');
+?>
+<article id="countries">
+    <h2>Contact Us</h2>
+    <h3>Have a recipe that you would like to add? Fill out this 
+        <a class="mainlinks" href="./recipeForm.php">recipe form</a></h3>
+    <h3>Want to subscribe for email updates from Regional Recipes about new 
+        recipes and other exciting information? Fill out this 
+        <a class="mainlinks" href="./subscriptionForm.php">subscription form</a></h3>
+    <h3>Have any comments for us? Fill out this 
+        <a class="mainlinks" href="./commentForm.php">comment form</a></h3>
+</article>
+<?php
+include('../footer.php');
+?>
 
