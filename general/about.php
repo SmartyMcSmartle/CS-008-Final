1fed19a9c6ffed78ff60b85b8453de0604445479
Julia Beatty <jybeatty@uvm.edu>
2017-12-06 19:24:48 -0500

    Added css, added information to about.php.

--- general/about.php
+++ general/about.php
@@ -1,9 +1,32 @@
-<!-- about us --!>
+
 <?php
+include ('../top.php');
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<article> 
+    <h3>Sarah McLaughlin</h3>
+    <figure class="img-left small">
+        <img src="../images/Liz.jpg" alt="Image of Sarah McLaughlin">
+        <br><span class="smalltext">Photo taken by UVM Career Center</span>
+    </figure>
+    <p>	Liz Bambury is a junior web developer for Regional Recipes. 
+        She is from Andover, Massachusetts and is currently a junior at the 
+        University of Vermont. She is pursuing a degree in Statistics with minors
+        in Computer Science and Global Health Anthropology. Hoping to to have a 
+        career in public health, Liz is interested in doing data analysis relating
+        to population health issues. Some of her favorite things to do include yoga,
+        skiing, watching documentaries, hiking in the summer, visiting her family’s 
+        cottage on Lake Superior in Michigan, as well as cooking with her friends 
+        and family. Cooking is something that Liz has fond memories of doing with 
+        her mom all throughout her childhood. After expressing such a love for 
+        cooking, Liz and her mom attended a class at the Cambridge Culinary 
+        School together to learn more technical skills. Exploring recipes from 
+        around the world combines both her interests in learning about our global
+        society as well as her love for cooking. Her favorite regional cuisine to 
+        cook and enjoy is Indian food. 
+    </p>
+</article>
 
+<?php
+include ('../footer.php');
+?>
