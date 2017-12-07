28ba7d5f0cfefb28875233a8361a8e9f57251535
Julia Beatty <jybeatty@uvm.edu>
2017-12-06 20:06:42 -0500

    Did some css and worked on about page

--- general/about.php
+++ general/about.php
@@ -9,7 +9,25 @@ include ('../top.php');
         <img src="../images/Liz.jpg" alt="Image of Sarah McLaughlin">
         <br><span class="smalltext">Photo taken by UVM Career Center</span>
     </figure>
-    <p>	Liz Bambury is a junior web developer for Regional Recipes. 
+    <p>Sarah is the lead web developer for Regional Recipes. She is from Needham, 
+        Massachusetts and is currently a freshman at the University of Vermont. 
+        She is pursuing a degree in Computer Science with a minor in Mathematics
+        and is considering a second minor in Electrical Engineering. In her 
+        future, Sarah hopes to work with artificial intelligence, computer 
+        vision, robotics, or some combination of the three. In her free time, 
+        Sarah loves playing ice hockey, irish step dancing, skiing, biking on 
+        the Cape Cod rail trail, reading, and playing cribbage. Cooking is an 
+        activity during which she and her father bonded when she was younger.
+        Often, she and her dad would make french toast, their favorite recipe, 
+        and her large, irish family prepares a traditional Christmas dinner 
+        together every year. </p>
+
+    <h3>Elizabeth Bambury</h3>
+    <figure class="img-left small">
+        <img src="../images/Liz.jpg" alt="Image of Sarah McLaughlin">
+             <br><span class="smalltext">Photo taken UVM Career Center</span>
+    </figure>
+    <p>Liz Bambury is a junior web developer for Regional Recipes. 
         She is from Andover, Massachusetts and is currently a junior at the 
         University of Vermont. She is pursuing a degree in Statistics with minors
         in Computer Science and Global Health Anthropology. Hoping to to have a 
@@ -23,8 +41,31 @@ include ('../top.php');
         School together to learn more technical skills. Exploring recipes from 
         around the world combines both her interests in learning about our global
         society as well as her love for cooking. Her favorite regional cuisine to 
-        cook and enjoy is Indian food. 
-    </p>
+        cook and enjoy is Indian food.</p>
+
+        <h3>Julia Beatty</h3>
+    <figure class="img-left small">
+        <img src="../images/Julia.jpg" alt="Image of Julia Beatty"
+             <br><span class="smalltext">Photo taken by Zoey Christensen</span>
+    </figure>
+    <p>Julia Beatty is a junior web designer for Regional Recipes. She is a 
+        senior at the University of Vermont. She is majoring in Chinese with 
+        a minor in Public Communication. She was born and raised in Anhui, 
+        China and currently lives in Vermont. Her mother, Kate Huntress was 
+        the chef-owner of Never Enough Thyme Catering on Mount Desert Island. 
+        Julia subscribes to cooking magazines, watches cooking shows, and attends
+        cooking classes on occasion. She grew up in a family that loves food. 
+        Julia was always helping in the kitchen mixing the brownie batter and 
+        chopping vegetables together. The kitchen is where she and her mom bonded. 
+        They love to prepare for elaborate holidays and would usually take 
+        those days off from work and just spend the day cooking and baking. 
+        She has a huge love for travel and food, as she believes that both 
+        encapsulate what it means to be from wherever you are from. Julia’s 
+        cooking skills are a representation of where she lived.  She loves to 
+        try new ingredients and wants to sample cuisine from different parts 
+        of the world. Julia would love to be able to travel and eat food. She 
+        wants to visit the parts of the world she’s never been to, and taste 
+        the authenticity of that region.</p>
 </article>
 
 <?php
