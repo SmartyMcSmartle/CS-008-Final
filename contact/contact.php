fbc7333647332086716eae16f42b53886b4d87a7
Elizabeth Bambury <Elizabeth>
2017-12-08 10:26:05 -0500

    Edited the css for the contact us page and made some changes to contact page relating to style

--- contact/contact.php
+++ contact/contact.php
@@ -3,13 +3,13 @@ include('../top.php');
 ?>
 <article id="countries">
     <h2>Contact Us</h2>
-    <h3>Have a recipe that you would like to add? Fill out this 
-        <a class="mainlinks" href="./recipeForm.php">recipe form</a></h3>
+    <h3>Have a recipe that you would like to add? Fill out:
+        <a class="mainlinks" href="./recipeForm.php">Recipe Form</a></h3>
     <h3>Want to subscribe for email updates from Regional Recipes about new 
-        recipes and other exciting information? Fill out this 
-        <a class="mainlinks" href="./subscriptionForm.php">subscription form</a></h3>
-    <h3>Have any comments for us? Fill out this 
-        <a class="mainlinks" href="./commentForm.php">comment form</a></h3>
+        recipes and other exciting information? Fill out:
+        <a class="mainlinks" href="./subscriptionForm.php">Subscription Form</a></h3>
+    <h3>Have any comments for us? Fill out:
+        <a class="mainlinks" href="./commentForm.php">Comment Form</a></h3>
 </article>
 <?php
 include('../footer.php');
