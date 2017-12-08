ecad0702177ec7e7e50c0856c08ddc93d8713dfd
Elizabeth Bambury <Elizabeth>
2017-12-07 21:44:12 -0500

    Added paragraph to header saying Take Your Tastebuds on a Journey

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <header>
     <img class="logo" alt="logo" src='../images/Logo.png'>
     <h1>Regional Recipes</h1>
+    <p style="font-style:italic"> Take your tastebuds on a journey</p>
 </header>
 <!-- End of Header -->
 
