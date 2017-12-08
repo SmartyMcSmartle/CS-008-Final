7fedb08e662657e4ea09bc29603ac5c0cb6b70d2
Julia Beatty <jybeatty@uvm.edu>
2017-12-07 22:05:23 -0500

    Updated the about.php and added some css for the about us page. Did css for faq page as well. Chnaged Julia's image.

--- general/about.php
+++ general/about.php
@@ -4,77 +4,70 @@ include ('../top.php');
 ?>
 
 <article> 
-    <h3>Sarah McLaughlin</h3>
-    <figure class="img-right small">
-        <a class="people" 
-           href="../general/about.php">
-            <img class="img-right" alt="Sarah McLaughlin" 
-                 src="../images/Liz.jpg">
-        </a>
+    <h2>About Us</h2>
+    
+    <h3 class="name">Sarah McLaughlin</h3>
+    <figure class="small">
+            <img class="img-right small" alt="Sarah McLaughlin" 
+                 src="../images/Sarah.jpg">
+        <p class="about">Sarah is the lead web developer for Regional Recipes. She is from Needham, 
+            Massachusetts and is currently a freshman at the University of Vermont. 
+            She is pursuing a degree in Computer Science with a minor in Mathematics
+            and is considering a second minor in Electrical Engineering. In her 
+            future, Sarah hopes to work with artificial intelligence, computer 
+            vision, robotics, or some combination of the three. In her free time, 
+            Sarah loves playing ice hockey, irish step dancing, skiing, biking on 
+            the Cape Cod rail trail, reading, and playing cribbage. Cooking is an 
+            activity during which she and her father bonded when she was younger.
+            Often, she and her dad would make french toast, their favorite recipe, 
+            and her large, irish family prepares a traditional Christmas dinner 
+            together every year. </p>
     </figure>
-    <p>Sarah is the lead web developer for Regional Recipes. She is from Needham, 
-        Massachusetts and is currently a freshman at the University of Vermont. 
-        She is pursuing a degree in Computer Science with a minor in Mathematics
-        and is considering a second minor in Electrical Engineering. In her 
-        future, Sarah hopes to work with artificial intelligence, computer 
-        vision, robotics, or some combination of the three. In her free time, 
-        Sarah loves playing ice hockey, irish step dancing, skiing, biking on 
-        the Cape Cod rail trail, reading, and playing cribbage. Cooking is an 
-        activity during which she and her father bonded when she was younger.
-        Often, she and her dad would make french toast, their favorite recipe, 
-        and her large, irish family prepares a traditional Christmas dinner 
-        together every year. </p>
 
-    <h3>Elizabeth Bambury</h3>
-    <figure class="img-right small">
-        <a class="people" 
-           href="../general/about.php">
-            <img class="img-right" alt="Elizabeth Bambury" 
+    <h3 class="name">Elizabeth Bambury</h3>
+    <figure class="small">
+            <img class="img-right small" alt="Elizabeth Bambury" 
                  src="../images/Liz.jpg">
-        </a>
+        <p class="about">Liz Bambury is a junior web developer for Regional Recipes. 
+            She is from Andover, Massachusetts and is currently a junior at the 
+            University of Vermont. She is pursuing a degree in Statistics with minors
+            in Computer Science and Global Health Anthropology. Hoping to to have a 
+            career in public health, Liz is interested in doing data analysis relating
+            to population health issues. Some of her favorite things to do include yoga,
+            skiing, watching documentaries, hiking in the summer, visiting her family’s 
+            cottage on Lake Superior in Michigan, as well as cooking with her friends 
+            and family. Cooking is something that Liz has fond memories of doing with 
+            her mom all throughout her childhood. After expressing such a love for 
+            cooking, Liz and her mom attended a class at the Cambridge Culinary 
+            School together to learn more technical skills. Exploring recipes from 
+            around the world combines both her interests in learning about our global
+            society as well as her love for cooking. Her favorite regional cuisine to 
+            cook and enjoy is Indian food.</p>
     </figure>
-    <p>Liz Bambury is a junior web developer for Regional Recipes. 
-        She is from Andover, Massachusetts and is currently a junior at the 
-        University of Vermont. She is pursuing a degree in Statistics with minors
-        in Computer Science and Global Health Anthropology. Hoping to to have a 
-        career in public health, Liz is interested in doing data analysis relating
-        to population health issues. Some of her favorite things to do include yoga,
-        skiing, watching documentaries, hiking in the summer, visiting her family’s 
-        cottage on Lake Superior in Michigan, as well as cooking with her friends 
-        and family. Cooking is something that Liz has fond memories of doing with 
-        her mom all throughout her childhood. After expressing such a love for 
-        cooking, Liz and her mom attended a class at the Cambridge Culinary 
-        School together to learn more technical skills. Exploring recipes from 
-        around the world combines both her interests in learning about our global
-        society as well as her love for cooking. Her favorite regional cuisine to 
-        cook and enjoy is Indian food.</p>
 
-    <h3>Julia Beatty</h3>
-    <figure class="img-right small">
-        <a class="people" 
-           href="../general/about.php">
-            <img class="img-right" alt="Julia Beatty" 
+    <h3 class="name">Julia Beatty</h3>
+    <figure class="small">
+            <img class="img-right small" alt="Julia Beatty" 
                  src="../images/Julia.jpg">
-        </a>
+        <p class="about">Julia Beatty is a junior web designer for Regional Recipes. She is a 
+            senior at the University of Vermont. She is majoring in Chinese with 
+            a minor in Public Communication. She was born and raised in Anhui, 
+            China and currently lives in Vermont. Her mother, Kate Huntress was 
+            the chef-owner of Never Enough Thyme Catering on Mount Desert Island. 
+            Julia subscribes to cooking magazines, watches cooking shows, and attends
+            cooking classes on occasion. She grew up in a family that loves food. 
+            Julia was always helping in the kitchen mixing the brownie batter and 
+            chopping vegetables together. The kitchen is where she and her mom bonded. 
+            They love to prepare for elaborate holidays and would usually take 
+            those days off from work and just spend the day cooking and baking. 
+            She has a huge love for travel and food, as she believes that both 
+            encapsulate what it means to be from wherever you are from. Julia’s 
+            cooking skills are a representation of where she lived.  She loves to 
+            try new ingredients and wants to sample cuisine from different parts 
+            of the world. Julia would love to be able to travel and eat food. She 
+            wants to visit the parts of the world she’s never been to, and taste 
+            the authenticity of that region.</p>
     </figure>
-    <p>Julia Beatty is a junior web designer for Regional Recipes. She is a 
-        senior at the University of Vermont. She is majoring in Chinese with 
-        a minor in Public Communication. She was born and raised in Anhui, 
-        China and currently lives in Vermont. Her mother, Kate Huntress was 
-        the chef-owner of Never Enough Thyme Catering on Mount Desert Island. 
-        Julia subscribes to cooking magazines, watches cooking shows, and attends
-        cooking classes on occasion. She grew up in a family that loves food. 
-        Julia was always helping in the kitchen mixing the brownie batter and 
-        chopping vegetables together. The kitchen is where she and her mom bonded. 
-        They love to prepare for elaborate holidays and would usually take 
-        those days off from work and just spend the day cooking and baking. 
-        She has a huge love for travel and food, as she believes that both 
-        encapsulate what it means to be from wherever you are from. Julia’s 
-        cooking skills are a representation of where she lived.  She loves to 
-        try new ingredients and wants to sample cuisine from different parts 
-        of the world. Julia would love to be able to travel and eat food. She 
-        wants to visit the parts of the world she’s never been to, and taste 
-        the authenticity of that region.</p>
 </article>
 
 <?php
--- general/faq.php
+++ general/faq.php
@@ -8,23 +8,23 @@ include('../top.php');
     <h2>Frequently Asked Questions</h2>
     
     <h3>How do I get to the individual recipe pages?</h3>
-    <p>You can start by going to the country page that you would like to explore 
+    <p class="faq">You can start by going to the country page that you would like to explore 
         from. All of our available recipies for that country will appear. Click 
         on the recipe image that you are interested in making. This will direct
         you to that specific recipe's page</p>
     <h3>How do I save a recipe?</h3>
-    <p>Once you are on the recipe page that you love, you can either copy and 
+    <p class="faq">Once you are on the recipe page that you love, you can either copy and 
     paste the recipe into a word document or bookmark it.</p>
     <h3>Can I use your recipes on my website/blog?</h3>
-    <p>Unfortunately, recipes cannot be reproduced or distributed to third parties,
+    <p class="faq">Unfortunately, recipes cannot be reproduced or distributed to third parties,
     including other websites for copyright reasons and as per our legal notices.</p>
     <h3>What do I get as a subscriber?</h3>
-    <p>As a subscriber to our Regional Recipes email list, you will have unlimited
+    <p class="faq">As a subscriber to our Regional Recipes email list, you will have unlimited
     access to all the content our website has to offer and monthly updates of new 
     recipes.</p>
     <h3>I have a recipe that I think you would be interested in including on the 
     site, how can I send it to you?</h3>
-    <p>We are always excited to try new recipes! For that reason, we have included 
+    <p class="faq">We are always excited to try new recipes! For that reason, we have included 
     a recipe form to be filled out if you would like to share a recipe with us. 
     The form can be found under the Contact Us portion of the navigation bar. We 
     may even include your recipe on our site in the future!</p>
